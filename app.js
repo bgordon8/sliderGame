16700e2aa419ba446dfd00dbe61a9561936e0a94
bgordon8 <gordonbrian16@gmail.com>
2021-04-27 16:23:06 -0700

    Adds dynamic app.js function for sliding width adds Stop btn functionality

--- app.js
+++ app.js
@@ -4,3 +4,37 @@ for (let i = 25; i > 0; i--) {
   slider.setAttribute("id", "slider" + i);
   document.getElementById("slide_game").append(slider);
 }
+
+function stopSliding(slider) {
+  var sliderCurrent = document.getElementById("slider" + slider);
+  var sliderAbove = document.getElementById("slider" + (slider + 1));
+  if (slider === 1) {
+    var sliderBelow = sliderCurrent;
+  } else {
+    var sliderBelow = document.getElementById("slider".concat(slider - 1));
+  }
+  var left = window.getComputedStyle(sliderCurrent).getPropertyValue("left");
+  sliderCurrent.classList.remove("animate");
+  sliderCurrent.style.left = left;
+  var width = parseInt(
+    window.getComputedStyle(sliderCurrent).getPropertyValue("width")
+  );
+  var leftBelow = parseInt(
+    window.getComputedStyle(sliderBelow).getPropertyValue("left")
+  );
+  var left = parseInt(left);
+  var difference = left - leftBelow;
+  var absDifference = Math.abs(difference);
+  if (difference > 0) {
+    left = left + absDifference;
+  } else {
+    left = left - difference;
+    sliderCurrent.style.left = left.toString().concat("px");
+  }
+  var offset = (width - absDifference).toString().concat("px");
+  sliderCurrent.style.width = offset;
+  sliderAbove.style.width = offset;
+  sliderAbove.style.visibilty = "visible";
+  var onclick = "stopSliding(" + (slider + 1) + ")";
+  document.getElementById("btn").setAttribute("onclick", onclick);
+}
